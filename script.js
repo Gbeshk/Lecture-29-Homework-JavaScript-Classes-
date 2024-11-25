59c4259d261d17f1e76319809998d4a46e4ec79d
Gbeshk <giorgibeshkenadze4@gmail.com>
2024-11-26 01:24:08 +0400

    vso

--- script.js
+++ script.js
@@ -76,8 +76,8 @@ class Car extends Vehicle {
   }
 }
 
-let car1 = new Car("Toyota", 180, "Corolla");
-let car2 = new Car("BMW", 220, "X5");
+let car1 = new Car("mersedesi", 200, "bolo");
+let car2 = new Car("bmw", 220, "boloswina");
 
 car1.describe();
 car2.describe();
@@ -90,23 +90,105 @@ class Product {
   }
 
   static filterByCategory(products, category) {
-    let filteredProducts = [];
-    for (i = 0; i < products.length(); i++) {
-      if (products[i].category === category) {
-        staticProducts.push(products[i]);
-      }
-    }
-    return staticProducts;
+    return products.filter((product) => product.category === category);
   }
 }
 
-let product1 = new Product("Laptop", 1000, "electronics");
-let product2 = new Product("Shirt", 30, "clothing");
+let product1 = new Product("iphone", 1000, "electronics");
+let product2 = new Product("bmw", 1500, "manqana");
+let product3 = new Product("samsung", 900, "electronics ");
 
-let product3 = new Product("Smartphone", 500, "electronics");
-
-let electronicsProducts = Product.filterByCategory(
+let electronics = Product.filterByCategory(
   [product1, product2, product3],
-  "electronics"
+  "electronics "
 );
-console.log(electronicsProducts);
+console.log(electronics);
+
+class Event {
+  constructor(name, date) {
+    this.name = name;
+    this.date = date;
+  }
+
+  isUpcoming() {
+    let date1 = new Date();
+    let date2 = new Date(this.date);
+    if (date1 > date2) {
+      return "momavalshia";
+    } else {
+      return "ar aris momavalshi";
+    }
+  }
+}
+
+const event1 = new Event("Conference", "2022-01-01");
+const event2 = new Event("Meeting", "2025-01-01");
+
+console.log(event1.isUpcoming());
+console.log(event2.isUpcoming());
+
+class Character {
+  constructor(name, health) {
+    this.name = name;
+    this.health = health;
+  }
+}
+
+class Warrior extends Character {
+  attack() {
+    this.health = this.health - 10;
+  }
+}
+
+let warrior1 = new Warrior("giorgi", 100);
+warrior1.attack();
+
+let warrior2 = new Warrior("luka", 100);
+warrior2.attack();
+console.log(warrior1.health);
+console.log(warrior2.health);
+
+class Task {
+  constructor(title, completed, priority) {
+    this.title = title;
+    this.completed = completed;
+    this.priority = priority;
+  }
+
+  toggleCompleted() {
+    this.completed = !this.completed;
+  }
+
+  static filterByCategory(tasks, priority) {
+    return tasks.filter((task) => task.priority === priority);
+  }
+}
+
+let task1 = new Task("todos awyoba", false, "Medium");
+task1.toggleCompleted();
+let task2 = new Task("todos awyoba2", true, "Important");
+task2.toggleCompleted();
+let task3 = new Task("todos awyoba3", true, "Low");
+task3.toggleCompleted();
+
+let filtered = Task.filterByCategory([task1, task2, task3], "Low");
+console.log(filtered);
+
+class Appointment {
+  constructor(title, date) {
+    this.title = title;
+    this.date = date;
+  }
+
+  isToday() {
+    let date1 = new Date();
+    let date2 = new Date(this.date);
+    return date1.toDateString() == date2.toDateString();
+  }
+}
+
+let appointment1 = new Appointment("todos gaketeba", "2025-11-26");
+let appointment2 = new Appointment("todos gaketeba 2", "2024-11-26");
+
+console.log(appointment1.isToday());
+console.log(appointment2.isToday());
